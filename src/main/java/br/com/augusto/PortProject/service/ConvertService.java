3e20ce34f9e5689b5849eea87220bde3c902cd0d
AkaAugustoNeves <augusto.nevesti@gmail.com>
2023-04-30 14:15:29 -0300

    service para converter

---
+++ src/main/java/br/com/augusto/PortProject/service/ConvertService.java
@@ -0,0 +1,19 @@
+package br.com.augusto.PortProject.service;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class ConvertService {
+
+	public static String  convertDataFormat(Date data) {
+		SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+		return formatter.format(data);		
+	}
+	
+	public static Date convertStringToDate(String dateString) throws ParseException {
+	    SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+	    return dateFormat.parse(dateString);
+	}
+	
+}
