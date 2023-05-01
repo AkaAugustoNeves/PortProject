11d0927cc6f27e07f191fa75a673236e09d73c0a
AkaAugustoNeves <augusto.nevesti@gmail.com>
2023-05-01 02:10:15 -0300

    metodos para escopo de pessoa

--- src/main/java/br/com/augusto/PortProject/service/PessoaService.java
+++ src/main/java/br/com/augusto/PortProject/service/PessoaService.java
@@ -3,9 +3,15 @@ package br.com.augusto.PortProject.service;
 import java.util.List;
 
 import br.com.augusto.PortProject.model.entity.Pessoa;
+import br.com.augusto.PortProject.model.enuns.Cargo;
+import br.com.augusto.PortProject.model.form.PessoaForm;
 
 public interface PessoaService {
 
 	List<Pessoa> buscarTodos();
 	
+	List<Pessoa> buscarPorCargo(Cargo cargo);
+	
+	Pessoa salvar(PessoaForm pessoa);
+
 }
